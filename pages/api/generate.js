3e6f19699bcd2d7b61f20d6cbfe2cd540d241181
billellevrai2022 <126887116+billellevrai2022@users.noreply.github.com>
2023-03-25 01:44:08 +0100

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,7 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix =
-  "Give me a one of a kind Instagram caption inspired by ";
+  "You're a Facebook advertising manager helping me set up a campaign for my product, you know all facebook target audience list Can you provide me with ten target audience ,  knowing that my product is : ";
 const generateAction = async (req, res) => {
   // Run first prompt
   // console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
